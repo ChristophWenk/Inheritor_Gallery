420d90a28fbbabdcf566b6b817ed67e9477ef496
dimitrimuralt <dimitri.muralt@students.fhnw.ch>
2019-06-01 19:12:35 +0200

    switched to logging, simplified code

--- inheritorgallery-core/src/main/java/javaparser/BasicParser.java
+++ inheritorgallery-core/src/main/java/javaparser/BasicParser.java
@@ -5,38 +5,43 @@ import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.NodeList;
 import com.github.javaparser.ast.body.*;
-import com.github.javaparser.ast.visitor.VoidVisitor;
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 
 public class BasicParser {
-    String className = "";
+
+    private static Logger logger = Logger.getLogger(BasicParser.class.getName());
+    //private static final String FILE_PATH = "inheritanceDummy/src/inheritanceDummyDemo/FahrzeugTest.java";
     private static final String FILE_PATH = "inheritorgallery-core/src/main/java/jshell/workingClasses/Person.java";
-            //"../inheritorgallery/core/SomeClass.java";
 
     public static void main(String[] args) throws Exception {
-
         CompilationUnit cu = StaticJavaParser.parse(new File(FILE_PATH));
         parse(cu);
+
     }
 
     private static void parse(CompilationUnit cu) {
+
         NodeList<TypeDeclaration<?>> ltd = cu.getTypes();
         Node node = ltd.get(0); // assuming no nested classes
         ClassOrInterfaceDeclaration coi = (ClassOrInterfaceDeclaration) node;
 
-        System.out.println(coi.getName());
-        System.out.println(coi.getFields());
-        List<MethodDeclaration> md = coi.getMethods();
-        for(MethodDeclaration m : md){
-            // m.getAccessSpecifier()+ " " + m.getType() + " " + m.getName()+ " "
-            System.out.println(m.getDeclarationAsString());
-        }
+        List<FieldDeclaration> fields = coi.getFields();
+        logger.info(String.valueOf(fields));
+
+        List<String> methods = coi.getMethods().stream()
+                .map(
+                        m -> m.getAccessSpecifier() + " " + m.getType() + " " + m.getName() + " " + m.getParameters())
+                .collect(Collectors.toList());
+        logger.info(String.valueOf(methods));
+
     }
-
-
-
 }
+
+
+
+
