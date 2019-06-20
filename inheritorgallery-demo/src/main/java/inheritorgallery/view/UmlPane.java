914e9741bc26fd11e683794a0200f669ad911981
dimitrimuralt <dimitri.muralt@studnets.fhnw.ch>
2019-06-20 10:37:22 +0200

    showing methods of each class in view

--- inheritorgallery-demo/src/main/java/inheritorgallery/view/UmlPane.java
+++ inheritorgallery-demo/src/main/java/inheritorgallery/view/UmlPane.java
@@ -3,7 +3,6 @@ package inheritorgallery.view;
 import inheritorgallery.view.util.UmlClass;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
-import javafx.scene.shape.Rectangle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import presentationmodel.uml.UmlPM;
@@ -16,7 +15,6 @@ public class UmlPane extends GridPane implements ViewMixin{
 
     private final UmlPM model;
 
-    private ArrayList<Label> labels = new ArrayList<>();
     private ArrayList<UmlClass> umlClasses = new ArrayList<>();
 
 
@@ -30,8 +28,7 @@ public class UmlPane extends GridPane implements ViewMixin{
     public void initializeControls() {
 
         for (int i=0 ; i < model.getClasses().size(); i++) {
-            labels.add(new Label());
-            umlClasses.add(new UmlClass());
+            umlClasses.add(new UmlClass(model.getClasses().get(i)));
         }
 
     }
@@ -39,7 +36,6 @@ public class UmlPane extends GridPane implements ViewMixin{
     @Override
     public void layoutControls() {
         for (int i=0 ; i < model.getClasses().size(); i++) {
-            add(labels.get(i), 0,i);
             add(umlClasses.get(i), 1,i);
         }
     }
@@ -47,9 +43,7 @@ public class UmlPane extends GridPane implements ViewMixin{
     @Override
     public void setupBindings() {
 
-        for (int i=0 ; i < model.getClasses().size(); i++) {
-            labels.get(i).textProperty().bind(model.getClasses().get(i).nameProperty());
-        }
+
     }
 
 }
--- inheritorgallery-demo/src/main/java/inheritorgallery/view/util/UmlClass.java
+++ inheritorgallery-demo/src/main/java/inheritorgallery/view/util/UmlClass.java
@@ -1,22 +1,62 @@
 package inheritorgallery.view.util;
 
 
+import inheritorgallery.view.ViewMixin;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
 import javafx.scene.shape.Rectangle;
+import presentationmodel.uml.ClassPM;
 
-public class UmlClass extends Region {
-    private Rectangle rectangle = new Rectangle();
+import java.util.ArrayList;
 
-    public UmlClass(){
+public class UmlClass extends VBox implements ViewMixin {
+    private final ClassPM model;
+    private Rectangle rectangle;
+    private Label classNameLabel;
+    private ArrayList<Label> methodLabels;
 
+    public UmlClass(ClassPM model){
+        this.model = model;
+        init();
+        //drawingPane.getChildren().addAll(rectangle);
+
+    }
+
+
+    @Override
+    public void initializeControls() {
+        rectangle = new Rectangle();
+        classNameLabel = new Label();
+        methodLabels = new ArrayList<>();
+
+        for (int i = 0; i < model.getMethods().size(); i++) {
+            methodLabels.add(new Label());
+        }
+    }
+
+    @Override
+    public void layoutControls() {
         rectangle.setWidth(30.0f);
         rectangle.setHeight(15.0f);
         rectangle.setX(50f);
         rectangle.setY(50f);
 
-        //drawingPane.getChildren().addAll(rectangle);
+        getChildren().addAll(rectangle, classNameLabel);
 
-        getChildren().add(rectangle);
+        for (int i=0 ; i < model.getMethods().size(); i++) {
+            getChildren().add(methodLabels.get(i));
+        }
     }
 
+    @Override
+    public void setupBindings() {
+        classNameLabel.textProperty().bind(model.nameProperty());
+
+        for (int i=0 ; i < model.getMethods().size(); i++) {
+            methodLabels.get(i).textProperty()
+                    .bind(model.getMethods().get(i).nameProperty());
+        }
+
+    }
 }
