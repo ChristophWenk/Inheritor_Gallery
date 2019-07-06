bc69a079707512bbcaf4160eeae2f5f9dc05a932
dimitrimuralt <dimitri.muralt@studnets.fhnw.ch>
2019-07-06 17:46:17 +0200

    InstanceStatePM creates Lists of ObjectPMs and ReferencePMs

--- inheritorgallery-core/src/main/java/presentationmodel/instance/InstanceStatePM.java
+++ inheritorgallery-core/src/main/java/presentationmodel/instance/InstanceStatePM.java
@@ -4,10 +4,13 @@ import exceptions.InvalidCodeException;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import service.jshell.JShellService;
+import service.jshell.ObjectDTO;
+import service.jshell.ReferenceDTO;
+import java.util.Optional;
 
 public class InstanceStatePM {
     private final ObservableList<String > commandHistory = FXCollections.observableArrayList();
-    private final ObservableList<ObjectPM > objectPMs = FXCollections.observableArrayList();
+    private final ObservableList<ObjectPM> objectPMs = FXCollections.observableArrayList();
     private final ObservableList<ReferencePM > referencePMs = FXCollections.observableArrayList();
 
     JShellService jShellService = JShellService.getInstance();
@@ -17,10 +20,32 @@ public class InstanceStatePM {
         try {
             String output = jShellService.getOutputAsString(jShellService.evaluateCode(input));
             commandHistory.addAll(input,output);
-
-
+            updateInstances();
         } catch (InvalidCodeException e) {
             commandHistory.add("Code could not be interpreted by JShell. Please verify the statement.");
+        }
+    }
+
+    private void updateInstances(){
+        objectPMs.removeAll();
+        for(ObjectDTO objectDTO : jShellService.getObjectDTOs() ){
+            objectPMs.add(new ObjectPM(
+                    objectDTO.getObjectId(),
+                    objectDTO.getObjectName()
+            ));
+        }
+        referencePMs.removeAll();
+        for(ReferenceDTO referenceDTO : jShellService.getReferenceDTOs()){
+            Optional<ObjectPM> ObjectPMPointedTo = objectPMs.stream()
+                    .filter(objectPM -> objectPM.getObjectId().equals(referenceDTO.getPointedToObject()))
+                    .findFirst();
+            if(ObjectPMPointedTo.isPresent()){
+                referencePMs.add(new ReferencePM(
+                        referenceDTO.getRefType(),
+                        referenceDTO.getRefName(),
+                        ObjectPMPointedTo.get()
+                ));
+            }
         }
     }
 
--- inheritorgallery-core/src/main/java/presentationmodel/instance/ReferencePM.java
+++ inheritorgallery-core/src/main/java/presentationmodel/instance/ReferencePM.java
@@ -1,4 +1,55 @@
 package presentationmodel.instance;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 public class ReferencePM {
+
+    private final StringProperty referenceType = new SimpleStringProperty();
+    private final StringProperty referenceName = new SimpleStringProperty();
+    private final ObjectProperty pointedToObject = new SimpleObjectProperty();
+
+    public ReferencePM(String referenceType, String referenceName, ObjectPM pointedToObject){
+        setReferenceType(referenceType);
+        setReferenceName(referenceName);
+        setPointedToObject(pointedToObject);
+    }
+
+    public String getReferenceType() {
+        return referenceType.get();
+    }
+
+    public StringProperty referenceTypeProperty() {
+        return referenceType;
+    }
+
+    public void setReferenceType(String referenceType) {
+        this.referenceType.set(referenceType);
+    }
+
+    public String getReferenceName() {
+        return referenceName.get();
+    }
+
+    public StringProperty referenceNameProperty() {
+        return referenceName;
+    }
+
+    public void setReferenceName(String referenceName) {
+        this.referenceName.set(referenceName);
+    }
+
+    public Object getPointedToObject() {
+        return pointedToObject.get();
+    }
+
+    public ObjectProperty pointedToObjectProperty() {
+        return pointedToObject;
+    }
+
+    public void setPointedToObject(Object pointedToObject) {
+        this.pointedToObject.set(pointedToObject);
+    }
 }
