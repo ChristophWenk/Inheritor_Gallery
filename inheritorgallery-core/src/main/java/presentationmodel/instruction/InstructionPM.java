f031e7644b6754b5807030aaa25f9e837b0b504e
ChristophWenk <christoph.wenk@students.fhnw.ch>
2019-08-13 14:29:18 +0200

    InstructionPM JavaDoc added

--- inheritorgallery-core/src/main/java/presentationmodel/instruction/InstructionPM.java
+++ inheritorgallery-core/src/main/java/presentationmodel/instruction/InstructionPM.java
@@ -8,13 +8,19 @@ import service.instruction.AsciiDocService;
 
 import java.nio.file.Path;
 
+/**
+ * Presentationmodel that stores the current state of the instruction text
+ */
 public class InstructionPM {
     AsciiDocService asciiDocService;
     private static Logger logger = LoggerFactory.getLogger(InstructionPM.class);
 
-
     private final StringProperty instructionText = new SimpleStringProperty();
 
+    /**
+     * Create the InstructionPM
+     * @param asciiDocService The service needed to convert an input instruction
+     */
     public InstructionPM(AsciiDocService asciiDocService) {
         this.asciiDocService = asciiDocService;
     }
