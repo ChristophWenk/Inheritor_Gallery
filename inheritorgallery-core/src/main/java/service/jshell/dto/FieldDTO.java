6492db51b9d2b1943d808abf309d61aa40976217
ChristophWenk <christoph.wenk@students.fhnw.ch>
2019-08-13 15:24:01 +0200

    FieldDTO and MethodDTO JavaDoc added

--- inheritorgallery-core/src/main/java/service/jshell/dto/FieldDTO.java
+++ inheritorgallery-core/src/main/java/service/jshell/dto/FieldDTO.java
@@ -2,6 +2,9 @@ package service.jshell.dto;
 
 import java.io.Serializable;
 
+/**
+ * Data transfer object for field
+ */
 public class FieldDTO implements Serializable {
     private static final long serialVersionUID = 1L;
     private final String declaringClass;
@@ -10,6 +13,14 @@ public class FieldDTO implements Serializable {
     private final String name;
     private final String value;
 
+    /**
+     * Create the FieldDTO
+     * @param declaringClass The class that declares the field
+     * @param modifier Modifier of the field (e.g. public)
+     * @param type Type of the field (e.g. static)
+     * @param name Name of the field
+     * @param value Value of the field
+     */
     public FieldDTO(String declaringClass,
                     String modifier,
                     String type,
