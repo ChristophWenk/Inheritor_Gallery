0cd5bc60a58e6710d9f22b3d0cda2777ef0fe06a
ChristophWenk <christoph.wenk@students.fhnw.ch>
2019-08-13 14:39:39 +0200

    EdgePM JavaDoc added

--- inheritorgallery-core/src/main/java/presentationmodel/uml/EdgePM.java
+++ inheritorgallery-core/src/main/java/presentationmodel/uml/EdgePM.java
@@ -3,11 +3,20 @@ package presentationmodel.uml;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
+/**
+ * Presentationmodel that stores the state of an edge (line in UML)
+ */
 public class EdgePM {
     private final StringProperty source = new SimpleStringProperty();
     private final StringProperty target = new SimpleStringProperty();
     private final StringProperty type = new SimpleStringProperty();
 
+    /**
+     * Create the EdgePM
+     * @param source Class where the edge is drawn from
+     * @param target Class where the edge is drawn to
+     * @param edgeType Type of the edge (e.g. interface)
+     */
     public EdgePM(String source, String target, String edgeType){
         setSource(source);
         setTarget(target);
